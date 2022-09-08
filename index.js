80c3e3ee51eb3f0dc87de2f74d84dd60b7f46175
yasminkassem66 <yasmin.abdellh@gmail.com>
2022-09-08 17:27:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+const express = require('express');
+const path = require('path');
+const app = express();
+// const port = process.env.PORT || 5000;
+const port = process.env.npm_package_config_port || 5000;
+app.use(express.static(path.resolve(`${__dirname}`, './')));
+app.get('/x', (req, res) => {
+    console.log("sdfgssd");
+    res.send('Yasssssssssssssssmin');
+});
+const server = app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+module.exports = server;
+// npm script is a tool to help automate 
+// how to create and use some basics scripts 
+// life cycle scripts vs custom scripts 
+// life cycle scripts=> test, start , stop  , pre post
+// custom scripts  => any others
---
+++ index.ts
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// const port = process.env.PORT || 5000;
+const port = process.env.npm_package_config_port || 5000;
+
+app.use(express.static(path.resolve(`${__dirname}`,'./')));
+
+app.get('/x',(req: any,res: { send: (arg0: string) => void; })=>{
+  console.log("sdfgssd");
+  res.send('Yasssssssssssssssmin')
+  
+});
+
+const server =app.listen(port, () =>
+console.log(`Server is listening on port ${port}...`)
+);
+module.exports=server;
+// npm script is a tool to help automate 
+// how to create and use some basics scripts 
+// life cycle scripts vs custom scripts 
+// life cycle scripts=> test, start , stop  , pre post
+// custom scripts  => any others
+// "build": "webpack",
+// "prestart": "npm run build",
+// "start": " node dist/app.js",
